7c473b37d571a720e9ce9b9eaefe207c7dc9f79c
Violet <violetcho@126.com>
2026-01-27 15:54:50 +0800

    feat: 修改 formatDate 函数以支持多种日期输入类型

--- mobile/lib/utils.ts
+++ mobile/lib/utils.ts
@@ -1,5 +1,5 @@
 // lib/utils.ts
-export function formatDate(date: Date) {
+export function formatDate(date: string | number | Date) {
     const data = new Date(date);
     return data.toLocaleDateString('en-US', {
         year: 'numeric',
