a5923efa29484dcf5b38a0a65871b9a8d591f49a
Violet <violetcho@126.com>
2026-02-09 17:36:21 +0800

    perf: 优化库版本

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,7 +1,5 @@
 import { View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
 import { Button } from 'react-native-paper';
-import { ScreenContent } from 'components/ScreenContent';
 
 export default function Index() {
   return (
